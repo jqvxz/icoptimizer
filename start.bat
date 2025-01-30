f44b3f25a2385ddfa09a7f2ed0774df53bd5ce0e
Javon <143961758+jqvxz@users.noreply.github.com>
2025-01-30 15:54:30 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -30,6 +30,7 @@ cls
 for /F "delims=" %%A in (header.txt) do (
     echo %RED%%%A%RESET%
 )
+echo.
 echo [!] This version of icoptimizer is outdated please install icoptimizer-remake
 cd ..\..
 echo.
